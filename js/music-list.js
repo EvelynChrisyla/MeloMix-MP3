2b171c324a0a5c6f88338515f483ed30100d19c2
Evelyn Chrisyla Valentina <valent2502@gmail.com>
2023-04-20 22:42:19 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,71 @@
+let allMusic=[
+    {
+        name: "Mahalini - Sial",
+        artist: "Mahalini",
+        img: "mahalini",
+        src:"sial"
+    },
+
+    {
+        name: "Cian Ducrot -  I'll be Waiting",
+        artist: "Cian Ducrot",
+        img: "cianducrot",
+        src:"BeWaiting"
+    },
+
+    {
+        name: "D4vd - Here With Me",
+        artist: "D4vd",
+        img: "david",
+        src:"HereWithMe"
+    },
+
+    {
+        name: "Raim Laode - Komang",
+        artist: "Raim Laode",
+        img: "komang",
+        src:"Komang"
+    },
+
+    {
+        name: "Mario G Klau - Semata Karena Mu",
+        artist: "Mario G Klau",
+        img: "mario",
+        src:"Sematakarenamu"
+    },
+
+    {
+        name: "Sza - Kill Bill",
+        artist: "Sza",
+        img: "sza",
+        src:"KillBill"
+    },
+
+    {
+        name: "New West - ThoseEyes",
+        artist: "New West",
+        img: "newwest",
+        src:"thoseeyes"
+    },
+
+    {
+        name: "Sabrina Carpenter - Nonsense",
+        artist: "Sabrina Carpenter",
+        img: "Sabrina Carpenter",
+        src:"Nonsense"
+    },
+
+    {
+        name: "Shania Twain - You’re Still The One",
+        artist: "Shania Twain",
+        img: "Shania Twain",
+        src:"YoureStillTheOne"
+    },
+
+    {
+        name: "Aziz Hedra - Somebody's Pleasure",
+        artist: "Aziz Hedra",
+        img: "Somebody's Pleasure",
+        src:"Somebody's Pleasure"
+    }
+]
